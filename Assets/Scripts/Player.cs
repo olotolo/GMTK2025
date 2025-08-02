3dc25feb2cf1339cd22b263cbdcd97f5b61fae92
MLeusch <marvin.m.leusch@gmail.com>
2025-08-02 02:16:33 +0200

    Added mirroring of the sprite for reverse direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float boostedFor;
+    [SerializeField] GameObject _sprite;
+
+    public void MirrorSprite()
+    {
+        _sprite.transform.localScale = new Vector3(-1f, 1f, 1f);
+    }
+    
 }
