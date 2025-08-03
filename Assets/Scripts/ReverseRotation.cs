f014ec96edd5946b964194aa196c7d7228505034
olotolo <olotolo@web.de>
2025-08-03 16:17:10 +0200

    Sounds

--- Assets/Scripts/ReverseRotation.cs
+++ Assets/Scripts/ReverseRotation.cs
@@ -17,5 +17,6 @@ public class ReverseRotation : MonoBehaviour
         {
             collision.GetComponent<Player>().MirrorSprite();
         }
+        AudioController.instance.Play("Bounce");
     }
 }
